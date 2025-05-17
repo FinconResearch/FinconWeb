ffbab678fbf94e7dea97aa0960d82d53b1e4a978
sidak <gdsd1524@gmail.com>
2025-05-18 05:00:16 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 import { NavbarDemo } from "@/components/layout/Header";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <NavbarDemo/>
         {children}
+        <Analytics/>
       </body>
     </html>
   );
